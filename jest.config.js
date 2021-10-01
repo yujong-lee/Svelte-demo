cdf1552a68c5337dd776a91f1a3b55ec5cac27b8
yujong-lee <yujonglee@g.skku.edu>
2021-10-01 22:59:10 +0900

    Testing setup

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    transform: {
+      '^.+\\.svelte$': 'svelte-jester',
+      '^.+\\.js$': 'babel-jest',
+    },
+    moduleFileExtensions: ['js', 'svelte'],
+    setupFilesAfterEnv: ['./jest.setup.ts', 'givens/setup.js', 'jest-plugin-context/setup.js',]
+  }
